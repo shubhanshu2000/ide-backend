ba35539f9eb94a0bff1d8240dcb86606e682d1ab
shubhanshu2000 <59794763+shubhanshu2000@users.noreply.github.com>
2022-12-15 14:12:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const ACTIONS = require("./Action");
-
+const env = require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
